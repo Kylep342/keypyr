b78508a18b3a773e70260af6343c0f4ee6e1f0f9
Kylep342 <kylep342@gmail.com>
2018-06-11 23:04:56 -0500

    Poor implementation of check_csvs

--- keypyr/check_csv.py
+++ keypyr/check_csv.py
@@ -12,18 +12,24 @@ def check_csv(path_to_csv_file, uniq_constraints, headers=True):
 
 
     """
-    seen_keys, duplicate_keys = {}, {}
+
     file_name = path_to_csv_file.split('/')[-1]
     table_name = file_name.replace('.csv', '')
+
+    seen_keys = {constraint['name'] : {} for constraint in uniq_constraints}
+    duplicate_keys = {'.'.join(table_name, constraint['name']) : [] for constraint in uniq_constraints}
+
 
     with open(path_to_csv_file, 'r') as csv_file:
         reader = csv.reader(csv_file)
         if headers:
             header_list = next(reader)
-        constraint_locs = [[header_list.index(col)] for constraint in uniq_constraints for col in constraint]
+        constraint_locs = [[header_list.index(col)] for constraint['column_names'] in uniq_constraints for col in constraint]
         for row in reader:
             for constraint in constraint_locs:
                 _key = (row[i] for i in constraint)
                 if _key not in seen_keys.keys():
-                    seen_keys[_key] = True
+                    seen_keys[constraint['name']][_key] = True
                 else:
+                    duplicate_keys['.'.join(table_name, constraint['name'])].insert(_key)
+    return duplicate_keys
